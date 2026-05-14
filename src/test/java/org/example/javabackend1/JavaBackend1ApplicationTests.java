7c38e2f0dcddf475897966036ffd1a6be4ae2f6e
Malte0v0 <malte.saarenpaa@yh.nackademin.se>
2026-05-14 12:50:17 +0200

    Initial commit

---
+++ src/test/java/org/example/javabackend1/JavaBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javabackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
