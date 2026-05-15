b21643f6d0eac9b195c3b48f31112dca5eca2998
simon molander <simon.molander@yh.nackademin.se>
2026-05-15 19:37:40 +0200

    RoomEntity

--- src/main/java/org/example/javabackend1/Room/RoomEntity.java
+++ src/main/java/org/example/javabackend1/Room/RoomEntity.java
@@ -1,4 +1,24 @@
 package org.example.javabackend1.Room;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "rooms")
+
+
 public class RoomEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+   private RoomType roomType;
+   private int extraBeds;
+
+
 }
