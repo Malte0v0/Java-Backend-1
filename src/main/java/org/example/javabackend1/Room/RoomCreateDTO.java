a34fe431bc6cb1d41bcb0943baed331df3e4e95a
Malte0v0 <malte.saarenpaa@yh.nackademin.se>
2026-05-17 20:30:32 +0200

    Fill out Room DTOs

--- src/main/java/org/example/javabackend1/Room/RoomCreateDTO.java
+++ src/main/java/org/example/javabackend1/Room/RoomCreateDTO.java
@@ -1,4 +1,14 @@
 package org.example.javabackend1.Room;
 
+import jakarta.validation.constraints.NotNull;
+
 public class RoomCreateDTO {
+    @NotNull
+    private Long roomId;
+
+    @NotNull
+    private RoomType roomType;
+
+    @NotNull
+    private int extraBeds;
 }
--- src/main/java/org/example/javabackend1/Room/RoomDTO.java
+++ src/main/java/org/example/javabackend1/Room/RoomDTO.java
@@ -1,4 +1,14 @@
 package org.example.javabackend1.Room;
 
+import jakarta.validation.constraints.NotNull;
+
 public class RoomDTO {
+    @NotNull
+    private Long roomId;
+
+    @NotNull
+    private RoomType roomType;
+
+    @NotNull
+    private int extraBeds;
 }
