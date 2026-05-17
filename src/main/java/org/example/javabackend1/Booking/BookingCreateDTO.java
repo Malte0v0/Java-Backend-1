09cae4ce2d9de56f9d0795d1f96d52e1ddb02cad
Malte0v0 <malte.saarenpaa@yh.nackademin.se>
2026-05-17 20:32:57 +0200

    Fill in Booking DTOs

--- src/main/java/org/example/javabackend1/Booking/BookingCreateDTO.java
+++ src/main/java/org/example/javabackend1/Booking/BookingCreateDTO.java
@@ -1,5 +1,27 @@
 package org.example.javabackend1.Booking;
 
-public class BookingCreateDTO {
+import jakarta.validation.constraints.NotNull;
+import org.example.javabackend1.Customer.CustomerEntity;
+import org.example.javabackend1.Room.RoomEntity;
 
+import java.time.LocalDateTime;
+
+public class BookingCreateDTO {
+    @NotNull
+    private CustomerEntity customer;
+
+    @NotNull
+    private RoomEntity room;
+
+    @NotNull
+    private LocalDateTime checkInDate;
+
+    @NotNull
+    private LocalDateTime checkOutDate;
+
+    @NotNull
+    private int numberOfGuests;
+
+    @NotNull
+    private BookingStatus status;
 }
--- src/main/java/org/example/javabackend1/Booking/BookingDTO.java
+++ src/main/java/org/example/javabackend1/Booking/BookingDTO.java
@@ -1,4 +1,30 @@
 package org.example.javabackend1.Booking;
 
+import jakarta.validation.constraints.NotNull;
+import org.example.javabackend1.Customer.CustomerEntity;
+import org.example.javabackend1.Room.RoomEntity;
+
+import java.time.LocalDateTime;
+
 public class BookingDTO {
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private CustomerEntity customer;
+
+    @NotNull
+    private RoomEntity room;
+
+    @NotNull
+    private LocalDateTime checkInDate;
+
+    @NotNull
+    private LocalDateTime checkOutDate;
+
+    @NotNull
+    private int numberOfGuests;
+
+    @NotNull
+    private BookingStatus status;
 }
