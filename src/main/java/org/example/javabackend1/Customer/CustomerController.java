7a8ac173bcf7811d97a2794c4eaa8bbd00fc7dbf
simon molander <simon.molander@yh.nackademin.se>
2026-05-26 17:14:46 +0200

    templates för custommer och controller

--- src/main/java/org/example/javabackend1/Customer/CustomerController.java
+++ src/main/java/org/example/javabackend1/Customer/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
+
+
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -15,8 +17,10 @@ public class CustomerController {
 
     @GetMapping
     public String mainMenu() {
-        return "/customers/customer";
+        return "customers/customer";
     }
+
+
 
     @GetMapping("/list")
     public String getAllCustomers(Model model) {
@@ -24,22 +28,42 @@ public class CustomerController {
         return "customers/list";
     }
 
+
+    // /customers/new
     @GetMapping("/new")
     public String showCreateForm(Model model) {
-        model.addAttribute("customerForm", new CustomerCreateDTO());
+        model.addAttribute("customer", new CustomerCreateDTO());
         return "customers/new";
+        // Visar
+        // templates/customers/new.html
     }
+
+
 
     @PostMapping("/new")
-    public String createCustomer(@ModelAttribute CustomerCreateDTO dto) {
+    public String createCustomer(@ModelAttribute("customer") CustomerCreateDTO dto) {
         this.customerService.create(dto);
+
         return "redirect:/customers/list";
     }
+    // Efter save:
+    // gå tillbaka till customerlistan.
 
+    // /customers/edit
     @GetMapping("/edit")
     public String showEditSearch() {
         return "customers/edit";
     }
+
+
+    // /customers/3/edit
+    @GetMapping("/{id}/edit")
+    public String showEditPage(@PathVariable Long id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        // templates/customers/edit.html
+        return "customers/edit";
+    }
+
 
     @GetMapping("/edit/find")
     public String findCustomerToEdit(@RequestParam Long id, Model model) {
@@ -47,24 +71,42 @@ public class CustomerController {
         return "customers/edit";
     }
 
+
+
     @PostMapping("/{id}/edit")
     public String updateCustomerById(@PathVariable Long id,
-                                    @ModelAttribute CustomerCreateDTO dto) {
+                                    @ModelAttribute("customer") CustomerCreateDTO dto) {
         customerService.update(id, dto);
         return "redirect:/customers/list";
     }
 
+
+    // /customers/delete
     @GetMapping("/delete")
     public String showDeleteSearch() {
+        return "redirect:/customers/list";
+    }
+
+    // /customers/3/delete
+    @GetMapping("/{id}/delete")
+    public String showDeletePage(
+            @PathVariable Long id, Model model) {
+        model.addAttribute("customer",
+                this.customerService.findById(id));
+// Hämtar customer som ska tas bort.
+
         return "customers/delete";
     }
 
+    // Körs när man söker customer att deletea via id.
     @GetMapping("/delete/find")
     public String findCustomerToDelete(@RequestParam Long id, Model model) {
         model.addAttribute("customer", this.customerService.findById(id));
         return "customers/delete";
     }
 
+
+    // Körs när delet knappen trycks.
     @PostMapping("/{id}/delete")
     public String deleteCustomerById(@PathVariable Long id) {
         this.customerService.delete(id);
