3c57d6c30493ab5b5c00fe6c20facda8af6fa52f
Malte0v0 <malte.saarenpaa@yh.nackademin.se>
2026-05-27 15:03:28 +0200

    Fix database persistency bug

--- src/main/java/org/example/javabackend1/Customer/CustomerRepository.java
+++ src/main/java/org/example/javabackend1/Customer/CustomerRepository.java
@@ -2,7 +2,6 @@ package org.example.javabackend1.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
